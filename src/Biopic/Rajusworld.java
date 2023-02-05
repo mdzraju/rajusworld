e9b75842ad46ccf4bd0f6ed07a2205b947fe7343
mdzraju <mdzraju28@gmail.com>
2023-02-05 13:19:50 -0500

    check the folder

---
+++ src/Biopic/Rajusworld.java
@@ -0,0 +1,10 @@
+package Biopic;
+
+public class Rajusworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Welcome to Raju's world");
+	}
+
+}
